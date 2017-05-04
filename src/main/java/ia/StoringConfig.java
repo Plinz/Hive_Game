61a9b2f10112d65d9bb03a281114676380f65f4e
mavax74 <m.raynal@no-log.org>
2017-05-04 17:51:58 +0200

    avancement de la storing config et ajout d'une ptite macro

--- src/main/java/ia/StoringConfig.java
+++ src/main/java/ia/StoringConfig.java
@@ -8,7 +8,6 @@ The pieces are associated to int between 0 and 10 in Consts.
  */
 package main.java.ia;
 
-import java.util.ArrayList;
 import java.util.List;
 import main.java.model.State;
 import main.java.model.Tile;
@@ -25,12 +24,20 @@ public class StoringConfig {
     public StoringConfig(State state) {
 
         List<List<List<Tile>>> board = state.getBoard().getBoard();
+        Tile current;
 
         /// first we find out the total amount of pieces in the game
         //->pieces on the board
         int total_pieces_nb = 0;
         for (int i = 0; i < board.size(); i++) {
-            total_pieces_nb += board.get(i).size();
+            for (int j = 0; j < board.get(i).size(); j++) {
+                for (int k = 0; k < board.get(i).get(j).size(); k++) {
+                    current = board.get(i).get(j).get(k);
+                    if ((current != null) && (current.getPiece() != null)) {
+                        total_pieces_nb++;
+                    }
+                }
+            }
         }
         // + pieces in hands of p1 & p2
         total_pieces_nb += state.getPlayer1().getInventory().size();
@@ -40,7 +47,6 @@ public class StoringConfig {
         config = new short[total_pieces_nb];
 
         //now filling up from the infos found in the board
-        Tile current;
         int index = 0;
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board.get(i).size(); j++) {
@@ -48,33 +54,83 @@ public class StoringConfig {
                 {
                     current = board.get(i).get(j).get(k);
 
-                    if ((current != null) && (current.getPiece() != null) && (current.getPiece().getTeam() == 1)) {
-                        index += total_pieces_nb / 2;
+                    //check if tile exists & if there's a piece in it
+                    if ((current != null) && (current.getPiece() != null)) {
+                        // get the piece byte from the team and name
+                        //first team & type
+                        //team
+                        if (current.getPiece().getTeam() == 1) {
+                            index += total_pieces_nb / 2;
+                        }
+                        //type
+                        switch (current.getPiece().getName()) {
+                            //Ants
+                            case Consts.ANT_NAME:
+                                if (config[index + Consts.ANT1] == 0) {
+                                    index += Consts.ANT1;
+                                } else if (config[index + Consts.ANT2] == 0) {
+                                    index += Consts.ANT2;
+                                } else if (config[index + Consts.ANT3] == 0) {
+                                    index += Consts.ANT3;
+                                } else {
+                                    //means there are 4 Ants with same color in the field -> should never happen
+                                    System.err.println("Erreur : 4 fourmis de la même couleur sur la board");
+                                }
+                                break;
+                            //Beetles
+                            case Consts.BEETLE_NAME:
+                                if (config[index + Consts.BEETLE1] == 0) {
+                                    index += Consts.BEETLE1;
+                                } else if (config[index + Consts.BEETLE2] == 0) {
+                                    index += Consts.BEETLE2;
+                                } else {
+                                    //means there are 3 beetles with same color in the field -> should never happen
+                                    System.err.println("Erreur : 3 scarabés de la même couleur sur la board");
+                                }
+                                break;
+                            //Queens
+                            case Consts.QUEEN_NAME:
+                                if (config[index + Consts.QUEEN] == 0) {
+                                    index += Consts.QUEEN;
+                                } else {
+                                    //means there are 2 queens with same color in the field -> should never happen
+                                    System.err.println("Erreur : 2 reines de la même couleur sur la board");
+                                }
+                                break;
+                            case Consts.SPIDER_NAME:
+                                if (config[index + Consts.SPIDER1] == 0) {
+                                    index += Consts.SPIDER1;
+                                } else if (config[index + Consts.SPIDER2] == 0) {
+                                    index += Consts.SPIDER2;
+                                } else {
+                                    //means there are 3 spiders with same color in the field -> should never happen
+                                    System.err.println("Erreur : 3 araignées de la même couleur sur la board");
+                                }
+                                break;
+                            case Consts.GRASSHOPPER_NAME:
+                                if (config[index + Consts.GRASSHOPPER1] == 0) {
+                                    index += Consts.GRASSHOPPER1;
+                                } else if (config[index + Consts.GRASSHOPPER2] == 0) {
+                                    index += Consts.GRASSHOPPER2;
+                                } else if (config[index + Consts.GRASSHOPPER3] == 0) {
+                                    index += Consts.GRASSHOPPER3;
+                                } else {
+                                    //means there are 4 grasshoppers with same color in the field -> should never happen
+                                    System.err.println("Erreur : 4 sauterelles de la même couleur sur la board");
+                                }
+                                break;
+                            default:
+                                System.err.println("Erreur : Nom de tuile inconnu présent dans la board");
+                                break;
+                        }
+                        this.setX(index, (byte) current.getX());
+                        this.setY(index, (byte) current.getY());
+                        this.setZ(index, (byte) current.getZ());
+                        this.setStuck(index, (current.isBlocked()==true?(byte)1:(byte)0));
+                        this.setIsVisited(index, (byte)0);
+                        this.setBool3(index,(byte) 1);///set to 1 to prevent mistakes with white queen on (0,0,0) unblocked
                     }
-                    // get the piece byte from the team and name
-                    switch (current.getPiece().getName()) {
-                        case Consts.ANT_NAME:
-                            if (config[index + Consts.ANT1] == 0) {
-                                index += Consts.ANT1;
-                            } else if (config[index + Consts.ANT2] == 0) {
-                                index += Consts.ANT2;
-                            } else if (config[index + Consts.ANT3] == 0) {
-                                index += Consts.ANT3;
-                            } else {
-                                //means there are 4 Ants with same color in the field -> should never happen
-                                System.err.println("Erreur : 4 fourmis de la même couleur sur la board");
-                            }
-                            break;
-                        case Consts.BEETLE_NAME:
 
-                            break;
-                        case Consts.QUEEN_NAME:
-
-                            break;
-                        case Consts.SPIDER_NAME:
-
-                            break;
-                    }
                 }
 
             }
@@ -94,15 +150,15 @@ public class StoringConfig {
         return (byte) ((config[index] & 0x0038) >> 3);
     }
 
-    public byte getStuck(int index) {
+    public byte getStuck(int index) { //tells if there's a bug atop that bug
         return (byte) ((config[index] & 0x0004) >> 2);
     }
 
-    public byte getIsVisited(int index) {
+    public byte getIsVisited(int index) { //needed to perform search algorithm (one hive check)
         return (byte) ((config[index] & 0x0002) >> 1);
     }
 
-    public byte getBool3(int index) {
+    public byte getBool3(int index) { //set to 1 during construction -> useless after
         return (byte) (config[index] & 1);
     }
 
@@ -141,7 +197,7 @@ public class StoringConfig {
         config[index] &= 0xFFFE;
         config[index] |= newBool3;
     }
-/*
+    /*
     // Tile to short -> Used to create the config from the state
     public short TileToShort(Tile tile) {
 
@@ -165,5 +221,5 @@ public class StoringConfig {
                 break;
         }
     }
-*/
+     */
 }
