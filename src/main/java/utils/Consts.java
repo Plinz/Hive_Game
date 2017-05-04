82930f4261f1c4b351fa857f24eabdbb565435d4
mavax74 <m.raynal@no-log.org>
2017-05-04 13:56:31 +0200

    ajout de Consts

---
+++ src/main/java/utils/Consts.java
@@ -0,0 +1,18 @@
+
+package main.java.utils;
+
+public class Consts {
+    
+    //MACROS PIECES
+    public static final int QUEEN = 0;
+    public static final int SPIDER1 = 1;
+    public static final int SPIDER2 = 2;
+    public static final int HOPPER1 = 3;
+    public static final int HOPPER2 = 4;
+    public static final int HOPPER3 = 5;
+    public static final int BEETLE1 = 6;
+    public static final int BEETLE2 = 7;
+    public static final int ANT1 = 8;
+    public static final int ANT2 = 9;
+    public static final int ANT3 = 10;
+}
