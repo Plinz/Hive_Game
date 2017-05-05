fc5b4b80b90dc29793d85bf7d282334ebff32a58
Plinz <an.duquennoy@gmail.com>
2017-05-05 13:17:25 +0200

    structure test

---
+++ src/test/java/model/BoardTest.java
@@ -0,0 +1,79 @@
+package test.java.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoardBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccept() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTileCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTileCoordInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemovePiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMovePiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNeighborsCoord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNeighborsTile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAboveAndBelow() {
+		fail("Not yet implemented");
+	}
+
+}
