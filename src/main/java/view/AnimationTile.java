6ce239b90279f6037bcf61f5db6dd29b4a2294ec
thomasduverney <thomas.duverney@etu.univ-grenoble-alpes.com>
2017-05-17 14:11:29 +0200

    Add first animation

--- src/main/java/view/AnimationTile.java
+++ src/main/java/view/AnimationTile.java
@@ -8,7 +8,10 @@ package main.java.view;
 import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Polygon;
 import javafx.util.Duration;
@@ -23,32 +26,39 @@ public class AnimationTile {
     private PathTransition pathAnimation;
     private Polygon polygon;
     private Pane panCanvas;
-    private TraducteurBoard traducteur;
     private Hexagon hex;
     
-    public AnimationTile(Pane panCanvas,TraducteurBoard traducteur){
+    public AnimationTile(Pane panCanvas){
                 
-        hex = new Hexagon(0.0,0.0); 
+        hex = new Hexagon(); 
+        hex.setxPixel(0.0);
+        hex.setyPixel(0.0);
+        hex.calculHex(); 
+        
         double x[] =  hex.getListXCoord();
         double y[] =  hex.getListYCoord();
         
         polygon = new Polygon();
-        
+        polygon.setFill(Color.RED);
         polygon.getPoints().addAll(new Double[]{
                     x[0], y[0],
                     x[1], y[1],
                     x[2], y[2],
                     x[3], y[3],
                     x[4], y[4],
-                    x[5], y[5] });     
+                    x[5], y[5] }); 
         
         pathAnimation = new PathTransition();
+        pathAnimation.setDuration(Duration.seconds(2));
+        pathAnimation.setNode(polygon);
+        
         pathAnimation.setOnFinished(new EventHandler<ActionEvent>() {
                 @Override
-                public void handle(ActionEvent t) {
+                public void handle(ActionEvent t) {        
                     panCanvas.getChildren().remove(polygon);
                 }
         });
+        this.panCanvas = panCanvas;
     };
 
     /**
@@ -71,12 +81,15 @@ public class AnimationTile {
     public Polygon getPolygon() {
         return polygon;
     }
-
-    /**
-     * @return the pathAnimation
-     */
-    public PathTransition getPathAnimation() {
-        return pathAnimation;
+    
+    public void setImagePolygon(Image image){     
+        polygon.setFill(new ImagePattern(image));
+    }
+        
+    public void play(){
+        panCanvas.getChildren().add(polygon);
+        pathAnimation.setPath(path);
+        pathAnimation.play();
     }
     
 }
