5f3012c529d6ad7cff6e983248be14a9b8652ea4
mavax74 <m.raynal@no-log.org>
2017-05-19 10:03:58 +0200

    ajout medium AI

---
+++ src/main/java/ia/MediumAI.java
@@ -0,0 +1,62 @@
+/*
+
+ */
+package main.java.ia;
+
+import java.util.Random;
+import main.java.engine.Core;
+import main.java.utils.Consts;
+
+public class MediumAI extends AI{
+    
+    public MediumAI(Core core) {
+        super(core);
+        this.heuristics = new EasyHeuristics(core);
+        this.choiceDuringOpening = new int[4];
+    }
+
+    @Override
+    public String getNextMove() {
+        this.AIPlayer = core.getCurrentPlayer();
+        if (core.getTurn() <=  7){
+            switch(core.getTurn()){
+                case 0 ://turn 1
+                case 1 :
+                    return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_1));
+                case 2 ://turn 2
+                case 3 :
+                    switch(choiceDuringOpening[0]){
+                        case 0 ://queen on turn 1
+                            return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_2_IF_QUEEN_ON_1));
+                        case 1 ://spider on turn 1
+                            return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_2_IF_SPIDER_ON_1));
+                        case 2 ://grasshopper on turn 1
+                            return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_2_IF_GRASSHOPPER_ON_1));
+                        case 3 ://beetle on turn 1
+                        case 4 ://ant on turn 1
+                            return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_2_IF_BETTLE_OR_ANT_ON_1));
+                        default :
+                            System.out.println("Erreur : Ne devrait jamais se produire");
+                    }
+                case 4 ://turn 3
+                case 5 :
+                    return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_3));
+                case 6 :
+                case 7 :
+                    if (!core.isQueenOnBoard(core.getCurrentPlayer())){
+                        return addPieceWherever(chooseAPiece(Consts.CHOOSE_QUEEN));
+                    } else {
+                        Random random = new Random();
+                        double rand = random.nextDouble();
+                        if (rand < Consts.MEDIUM_TURN_4_CHOOSE_TO_ADD)
+                            return addPieceWherever(chooseAPiece(Consts.MEDIUM_ADD_TURN_4));
+                        else
+                            return movePieceNearOpponent(chooseWhateverFreeTileOnBoard(core.getCurrentPlayer()));
+                    }
+                default :
+                    System.out.println("Erreur : Ne devrait jamais se produire");
+            }
+        }
+        return null;
+    }
+}
