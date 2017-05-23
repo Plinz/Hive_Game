56eb865152d00cec400e02a75ff9ad93873289dc
Cieldara <benjamin.gontard2912@gmail.com>
2017-05-23 15:08:29 +0200

    Changement du bouton de fin de partie

--- src/main/java/controller/GameScreenController.java
+++ src/main/java/controller/GameScreenController.java
@@ -798,7 +798,7 @@ public class GameScreenController implements Initializable {
         ButtonType restart = new ButtonType("Relancer la partie", ButtonBar.ButtonData.LEFT);
         ButtonType mainMenu = new ButtonType("Retourner au menu principal", ButtonBar.ButtonData.OTHER);
         ButtonType cancel = new ButtonType("Fermer", ButtonBar.ButtonData.RIGHT);
-        if(core.getMode() != Consts.PVEX || core.getMode() != Consts.EXVP)
+        if(core.getMode() != Consts.PVEX && core.getMode() != Consts.EXVP)
             popup.getDialogPane().getButtonTypes().addAll(restart, mainMenu,cancel);
         else
             popup.getDialogPane().getButtonTypes().addAll(mainMenu);
