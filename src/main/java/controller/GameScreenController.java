b9c0a754c0fc02e284a7c782446bb0a2f8ed7427
Cieldara <hyperu@DESKTOP-D67G505>
2017-05-29 22:29:00 +0200

    La c'est fix pour de bon

--- src/main/java/controller/GameScreenController.java
+++ src/main/java/controller/GameScreenController.java
@@ -875,7 +875,7 @@ public class GameScreenController implements Initializable {
             List<Column> col = core.getBoard().getBoard();
             for(int i = 0; i< col.size();i++){
                 if(col.size() > 0){
-                    if(col.get(i).size() > 0){
+                    if(col.get(i).size() > 1){
                         Box box = col.get(i).get(1);
                         if(box.size() != 0 && box.get(0).getPiece() != null){
                             nbPieces+= box.size();
